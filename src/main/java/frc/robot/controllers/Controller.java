37306447529e2a043ed08b3a928eed17b9663976
cinarkagan <68916102+cinarkagan@users.noreply.github.com>
2026-04-10 12:26:15 +0300

    Delete src directory

--- src/main/java/frc/robot/controllers/Controller.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.controllers;
-
-import edu.wpi.first.wpilibj2.command.Command;
-
-public interface Controller {
-    void getInitializeFunction();
-    void simulationPeriodic();
-    void periodic();
-}
